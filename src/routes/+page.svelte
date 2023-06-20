230ac75ef926b55d9ef1227b720e5066a55feb5e
Charles Maverick Herrera <90278189+CMH7@users.noreply.github.com>
2023-06-20 20:13:18 +0800

    hmmm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Footer from "../lib/components/Footer.svelte";
-	import Navbar from "../lib/components/navbar.svelte";
+	import Navbar from "../lib/components/Navbar.svelte";
 </script>
 
 
